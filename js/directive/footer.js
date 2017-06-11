fbaaebc59bb453ba9f6296e5c0e2adfcc5fed3f6
LY060708 <429209746@qq.com>
2017-06-11 16:53:34 +0800

    22

---
+++ js/directive/footer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 42920 on 2017/6/11.
+ */
+var app = angular.module("footerDirectiveModule",[]);
+app.directive("footerHtml",function () {
+    return{
+        restrict:"AE",
+        replace:true,
+        templateUrl:"footer.html"
+
+    }
+
+})
